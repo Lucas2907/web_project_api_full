e208a155e144d410f687d6125f669f193bbc7694
Lucas <lucaspasagarcia29@gmail.com>
2025-12-09 21:49:19 +0000

    Changes data sent to the client

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -16,8 +16,8 @@ module.exports.getCards = (req, res, next) => {
 module.exports.createCards = (req, res, next) => {
   const { name, link, owner = req.user._id } = req.body;
   Card.create({ name, link, owner })
-    .then((card) => {
-      res.status(201).send({ data: card });
+    .then(() => {
+      res.status(201).send({ message: "Card criado com sucesso" });
     })
     .catch(next);
 };
@@ -43,27 +43,36 @@ module.exports.deleteCard = (req, res, next) => {
 
 module.exports.likeCard = (req, res, next) => {
   const userId = req.user._id;
-
-  Card.findByIdAndUpdate(
-    req.params.cardId,
-    { $addToSet: { likes: userId } },
-    { new: true }
-  )
-    .then((card) => {
-      res.send(card);
+  Card.findById(req.params.cardId)
+    .then(async (card) => {
+      if (!card) {
+        throw new NotFoundError("CardId não encontrado");
+      }
+      await Card.findByIdAndUpdate(
+        req.params.cardId,
+        { $addToSet: { likes: userId } },
+        { new: true }
+      ).then(() => {
+        res.send({ message: "Card curtido com sucesso" });
+      });
     })
     .catch(next);
 };
 
 module.exports.deleteLike = (req, res, next) => {
   const userId = req.user._id;
-  Card.findByIdAndUpdate(
-    req.params.cardId,
-    { $pull: { likes: userId } },
-    { new: true }
-  )
-    .then((card) => {
-      res.send(card);
+  Card.findById(req.params.cardId)
+    .then(async (card) => {
+      if (!card) {
+        throw new NotFoundError("CardId não encontrado");
+      }
+      await Card.findByIdAndUpdate(
+        req.params.cardId,
+        { $addToSet: { likes: userId } },
+        { new: true }
+      ).then(() => {
+        res.send({ message: "Card descurtido com sucesso" });
+      });
     })
     .catch(next);
 };
